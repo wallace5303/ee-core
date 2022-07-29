a1e2a90129e338ca7774d95e4277323b02804f73
gaoshuaixing <gaoshuaixing@bilibili.com>
2022-07-29 15:08:41 +0800

    一些工具函数修复

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
 const constant = require('../lib/constant');
 const convert = require('koa-convert');
@@ -8,51 +7,21 @@ const is = require('is-type-of');
 const co = require('co');
 const utility = require('utility');
 const eis = require('electron-is');
+const utilsCommon = require('./common');
 
 /**
  * 创建文件夹
  */
-exports.mkdir = function(dirpath, dirname) {
-  // 判断是否是第一次调用
-  if (typeof dirname === 'undefined') {
-    if (fs.existsSync(dirpath)) {
-      return;
-    }
-    this.mkdir(dirpath, path.dirname(dirpath));
-  } else {
-    // 判断第二个参数是否正常，避免调用时传入错误参数
-    if (dirname !== path.dirname(dirpath)) {
-      this.mkdir(dirpath);
-      return;
-    }
-    if (fs.existsSync(dirname)) {
-      fs.mkdirSync(dirpath);
-    } else {
-      this.mkdir(dirname, path.dirname(dirname));
-      fs.mkdirSync(dirpath);
-    }
-  }
-};
+exports.mkdir = function (dirpath, dirname) {
+  return utilsCommon.mkdir(dirpath, dirname);
+}
 
 /**
  * 修改文件权限
  */
-exports.chmodPath = function(path, mode) {
-  let files = [];
-  if (fs.existsSync(path)) {
-    files = fs.readdirSync(path);
-    files.forEach((file, index) => {
-      const curPath = path + '/' + file;
-      if (fs.statSync(curPath).isDirectory()) {
-        this.chmodPath(curPath, mode); // 递归删除文件夹
-      } else {
-        fs.chmodSync(curPath, mode);
-      }
-    });
-    fs.chmodSync(path, mode);
-  }
-};
-
+exports.chmodPath = function (path, mode) {
+  return utilsCommon.chmodPath(path, mode);
+}
 
 /**
  * 获取项目根目录package.json
@@ -97,10 +66,7 @@ exports.getEggConfig = function() {
  * 获取 数据库存储路径
  */
 exports.getStorageDir = function() {
-  const cdb = this.getCoreDB();
-  const dirPath = cdb.getStorageDir();
-
-  return dirPath;
+  return utilsCommon.getStorageDir();
 }
 
 /**
@@ -118,7 +84,8 @@ exports.getAppUserDataDir = function() {
  * 获取 日志目录
  */
 exports.getLogDir = function() {
-  const logPath = path.join(this.getAppUserDataDir(), 'logs');
+  const cdb = this.getCoreDB();
+  const logPath = cdb.getItem('config').logger.dir;
   return logPath;
 }
 
@@ -180,28 +147,5 @@ exports.middleware = function (fn) {
  * 版本号比较
  */
 exports.compareVersion = function (v1, v2) {
-  v1 = v1.split('.')
-  v2 = v2.split('.')
-  const len = Math.max(v1.length, v2.length)
-
-  while (v1.length < len) {
-    v1.push('0')
-  }
-  while (v2.length < len) {
-    v2.push('0')
-  }
-  
-  for (let i = 0; i < len; i++) {
-    const num1 = parseInt(v1[i])
-    const num2 = parseInt(v2[i])
-
-    if (num1 > num2) {
-      return 1
-    } else if (num1 < num2) {
-      return -1
-    }
-  }
-
-  return 0
+  return utilsCommon.compareVersion(v1, v2);
 }
-
