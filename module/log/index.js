8697df7d59c750bf0af13496e7eb44299bcba91a
哆啦好梦 <530353222@qq.com>
2023-02-14 17:51:07 +0800

    module log

--- module/log/index.js
+++ module/log/index.js
@@ -1,13 +1,37 @@
-const logger = require('./logger');
+const Logger = require('./logger');
+const EELoggers = Symbol('EeApplication#EELoggers');
+
 const Log = {};
 
 /**
  * 创建日志实例
  */
 Log.create = function (config) {
-  const eeLoggers = logger.getInstance(config);
+  const eeLoggers = new Logger(config);
 
   return eeLoggers;
+};
+
+/**
+ * logger
+ */
+Log.logger = function () {
+  if (!this[EELoggers]) {
+    this[EELoggers] = new Logger();
+  }
+
+  return this[EELoggers]['logger'] || null;
+};
+
+/**
+ * coreLogger
+ */
+Log.coreLogger = function () {
+  if (!this[EELoggers]) {
+    this[EELoggers] = new Logger();
+  }
+
+  return this[EELoggers]['coreLogger'] || null;
 };
 
 module.exports = Log;
--- module/log/logger.js
+++ module/log/logger.js
@@ -4,20 +4,8 @@ const assert = require('assert');
 const storage = require('../storage');
 
 class Logger {
-  constructor (config) {
+  constructor (config = {}) {
     debug('Loaded logger');
-
-    assert(Object.keys(config).length != 0, `logger config is null`);
-    this.eggLogger = this.init(config);
-  }
-
-  /**
-   * 单例
-   */
-  static getInstance (config = {}) {
-    if (typeof this.instance === 'object') {
-      return this.instance.eggLogger;
-    }
 
     if (Object.keys(config).length == 0) {
       const sysConfig = this._getCoreDB().getItem('config');
@@ -26,19 +14,17 @@ class Logger {
         customLogger: sysConfig.customLogger || {}
       });
     }
-
     console.log('log---------', config);
 
-    this.instance = new Logger(config);
-
-    // 返回egg-logger实例
-    return this.instance.eggLogger;
+    this.loggers = this._init(config);
   }
 
   /**
    * 初始化模块
    */
-  init(config) {
+  _init(config) {
+    assert(Object.keys(config).length != 0, `logger config is null`);
+
     const loggers = new Loggers(config);
     loggers.coreLogger.info('[ee-core:logger] init all loggers with options: %j', config);
 
@@ -51,7 +37,7 @@ class Logger {
   _getCoreDB() {
     const coreDB = storage.connection('system');
     return coreDB;
-  }
+  }  
 }
 
 module.exports = Logger;
