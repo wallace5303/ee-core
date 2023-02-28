2ed50ec83ee6321a52e2e22901cd5035857d4ba1
哆啦好梦 <530353222@qq.com>
2023-02-28 19:36:35 +0800

    job

--- module/jobs/child/app.js
+++ module/jobs/child/app.js
@@ -1,51 +1,43 @@
 
 const Exception = require('ee-core/module/exception');
 Exception.start();
-
 const Loader = require('ee-core/module/loader');
 const Log = require('ee-core/module/log');
 
 class ChildApp {
   constructor() {
-    const args = process.argv[2];
-    this.opt = args ? JSON.parse(args) : {};
-
-    //this._initEvents();
+    this._initEvents();
   }
 
   /**
    * 初始化事件监听
    */
-  // _initEvents() {
-  //   Log.info('[ee-core] [child-process] init Events');
+  _initEvents() {
+    Log.info('[ee-core] [child-process] init Events');
 
-  //   process.on('disconnect', () => {
-  //     Log.coreLogger.info(`[ee-core] [module/message/childMessage] child process disconnected:${process.pid} !`);
-  //   });
-  //   process.on('exit', () => {
-  //     Log.coreLogger.info(`[ee-core] [module/message/childMessage] child process exited:${process.pid} !`);
-  //   });
-  //   process.on('message', this._handleMessage.bind(this));
-  // }
+    process.on('disconnect', () => {
+      Log.coreLogger.info(`[ee-core] [module/message/childMessage] child process disconnected:${process.pid} !`);
+    });
+    process.on('exit', () => {
+      Log.coreLogger.info(`[ee-core] [module/message/childMessage] child process exited:${process.pid} !`);
+    });
+    process.on('message', this._handleMessage.bind(this));
+  }
 
   /**
    * 监听消息
    */
-  // _handleMessage(message) {
-  //   Log.coreLogger.info(`[ee-core] [module/message/childMessage] Received a message ${message} from the mainProcess`);
+  _handleMessage(message) {
+    Log.coreLogger.info(`[ee-core] [module/message/childMessage] Received a message ${message} from the mainProcess`);
 
-  //   this.run(message);
-  // }
+    this.run(message);
+  }
 
-  run() {
-    Log.info('[ee-core] [child-process] run');
-    
-    // const jobFile = this.opt.jobPath;
-    // Loader.loadJobFile(jobFile);
+  run(msg = {}) {
+    Log.coreLogger.info('[ee-core] [child-process] run');
 
-    //Loader.loadJobFile(msg.jobPath, msg.params);
+    Loader.loadJobFile(msg.jobPath, msg.params);
   }
 }
 
-const app = new ChildApp()
-app.run();
+new ChildApp();
