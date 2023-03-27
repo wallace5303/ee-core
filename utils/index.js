345a95b4035ffff67481d1ef10433a39e029e0a0
哆啦好梦 <530353222@qq.com>
2023-03-27 19:29:18 +0800

    mac address

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,6 @@
+"use strict";
+
+const os = require("os");
 const path = require('path');
 const Ps = require('../ps');
 const UtilsJson = require('./json');
@@ -10,6 +13,49 @@ exports.getPackage = function() {
   
   return json;
 };
+
+/**
+ * Get the first proper MAC address
+ * @param iface If provided, restrict MAC address fetching to this interface
+ */
+exports.getMAC = function(iface) {
+  const zeroRegex = /(?:[0]{1,2}[:-]){5}[0]{1,2}/;
+  const list = os.networkInterfaces();
+  if (iface) {
+      const parts = list[iface];
+      if (!parts) {
+          throw new Error(`interface ${iface} was not found`);
+      }
+      for (const part of parts) {
+          if (zeroRegex.test(part.mac) === false) {
+              return part.mac;
+          }
+      }
+      throw new Error(`interface ${iface} had no valid mac addresses`);
+  }
+  else {
+      for (const [key, parts] of Object.entries(list)) {
+          // for some reason beyond me, this is needed to satisfy typescript
+          // fix https://github.com/bevry/getmac/issues/100
+          if (!parts)
+              continue;
+          for (const part of parts) {
+              if (zeroRegex.test(part.mac) === false) {
+                  return part.mac;
+              }
+          }
+      }
+  }
+  throw new Error('failed to get the MAC address');
+}
+
+/**
+ * Check if the input is a valid MAC address
+ */
+exports.isMAC = function(macAddress) {
+  const macRegex = /(?:[a-z0-9]{1,2}[:-]){5}[a-z0-9]{1,2}/i;
+  return macRegex.test(macAddress);
+}
 
 
 
