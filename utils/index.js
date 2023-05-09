c28fd1fb02cab33259edbb486a12e1958374af17
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-05-09 10:45:46 +0800

    machineIdSync & machineId

--- utils/index.js
+++ utils/index.js
@@ -3,8 +3,25 @@
 const os = require("os");
 const path = require('path');
 const fs = require('fs');
+const { exec, execSync } = require('child_process');
+const { createHash } = require('crypto');
 const Ps = require('../ps');
 const UtilsJson = require('./json');
+
+// machine id
+const { platform } = process;
+const win32RegBinPath = {
+  native: '%windir%\\System32',
+  mixed: '%windir%\\sysnative\\cmd.exe /c %windir%\\System32'
+};
+const MachineGuid = {
+  darwin: 'ioreg -rd1 -c IOPlatformExpertDevice',
+  win32: `${win32RegBinPath[isWindowsProcessMixedOrNativeArchitecture()]}\\REG.exe ` +
+      'QUERY HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography ' +
+      '/v MachineGuid',
+  linux: '( cat /var/lib/dbus/machine-id /etc/machine-id 2> /dev/null || hostname ) | head -n 1 || :',
+  freebsd: 'kenv -q smbios.system.uuid || sysctl -n kern.hostuuid'
+};
 
 /**
  * 获取项目根目录package.json
@@ -68,5 +85,76 @@ exports.isEncrypt = function(basePath) {
   }
   return false;
 }
+
+/**
+ * get machine id
+ */
+exports.machineIdSync = function(original) {
+  let id = expose(execSync(MachineGuid[platform]).toString());
+  return original ? id : hash(id);
+}
+
+/**
+ * get machine id (promise)
+ * original <Boolean>, If true return original value of machine id, otherwise return hashed value (sha-256), default: false
+ */
+exports.machineId = function(original) {
+  return new Promise((resolve, reject) => {
+    return exec(MachineGuid[platform], {}, (err, stdout, stderr) => {
+      if (err) {
+        return reject(
+            new Error(`Error while obtaining machine id: ${err.stack}`)
+        );
+      }
+      let id = expose(stdout.toString());
+      return resolve(original ? id : hash(id));
+    });
+  });
+}
+
+function isWindowsProcessMixedOrNativeArchitecture() {
+  // detect if the node binary is the same arch as the Windows OS.
+  // or if this is 32 bit node on 64 bit windows.
+  if(process.platform !== 'win32') {
+    return '';
+  }
+  if( process.arch === 'ia32' && process.env.hasOwnProperty('PROCESSOR_ARCHITEW6432') ) {
+    return 'mixed';
+  }
+  return 'native';
+}
+
+function hash(guid) {
+  return createHash('sha256').update(guid).digest('hex');
+}
+
+function expose(result) {
+  switch (platform) {
+    case 'darwin':
+      return result
+        .split('IOPlatformUUID')[1]
+        .split('\n')[0].replace(/\=|\s+|\"/ig, '')
+        .toLowerCase();
+    case 'win32':
+      return result
+        .toString()
+        .split('REG_SZ')[1]
+        .replace(/\r+|\n+|\s+/ig, '')
+        .toLowerCase();
+    case 'linux':
+      return result
+        .toString()
+        .replace(/\r+|\n+|\s+/ig, '')
+        .toLowerCase();
+    case 'freebsd':
+      return result
+        .toString()
+        .replace(/\r+|\n+|\s+/ig, '')
+        .toLowerCase();
+    default:
+      throw new Error(`Unsupported platform: ${process.platform}`);
+  }
+}
+
 
 
