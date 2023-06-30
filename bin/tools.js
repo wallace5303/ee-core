b0751bfae0b2112d1f7dc59afb7728e4ede53c36
Canim <840054486@qq.com>
2023-06-30 10:52:53 +0800

    新增ee-core icon 命令 方便生成 logo 相关资源

--- bin/tools.js
+++ bin/tools.js
@@ -2,6 +2,7 @@
 
 const replaceDist = require('../tools/replaceDist');
 const encrypt = require('../tools/encrypt');
+const iconGen = require('../tools/iconGen');
 
 // argv
 const args = process.argv;
@@ -19,4 +20,8 @@ if (cmd == 'encrypt') {
 
 if (cmd == 'clean') {
   encrypt.clean();
+}
+
+if (cmd == 'icon') {
+  iconGen.run();
 }
