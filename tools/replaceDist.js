d11e31667c1677b9c12588b5470a7d0dc23ce923
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-07-03 17:41:11 +0800

    icongen & rd

--- tools/replaceDist.js
+++ tools/replaceDist.js
@@ -13,7 +13,7 @@ const fsPro = require('fs-extra');
   /**
    * 执行
    */  
-  run () {
+  run() {
     console.log('[ee-core] [tools/rd] 开始移动资源');
     const homeDir = process.cwd();
 
@@ -43,19 +43,57 @@ const fsPro = require('fs-extra');
       return
     }
     
-    // 复制到ee资源目录
+    // 清空历史资源 并 复制到ee资源目录
     const eeResourceDir = path.join(homeDir, 'public', 'dist');
-
-    // 清空历史资源
-    fs.rmdirSync(eeResourceDir, {recursive: true});
+    if (!fs.existsSync(eeResourceDir)) {
+      fs.mkdirSync(eeResourceDir, {recursive: true, mode: 0o777});
+    }
+    this._rmFolder(eeResourceDir);
     console.log('[ee-core] [tools/rd] 清空历史资源:', eeResourceDir);
 
     fsPro.copySync(sourceDir, eeResourceDir);
     console.log('[ee-core] [tools/rd] 复制资源到:', eeResourceDir);
-
     console.log('[ee-core] [tools/rd] 结束');
+  },
+
+  /**
+   * 删除文件夹
+   */
+  _rmFolder(folder) {
+    const nodeVersion = (process.versions && process.versions.node) || null;
+    if (nodeVersion && this._compareVersion(nodeVersion, '14.14.0') == 1) {
+      fs.rmSync(folder, {recursive: true});
+    } else {
+      fs.rmdirSync(folder, {recursive: true});
+    }
+  },
+
+  /**
+   * 版本号比较
+   */
+  _compareVersion(v1, v2) {
+    v1 = v1.split('.')
+    v2 = v2.split('.')
+    const len = Math.max(v1.length, v2.length)
+
+    while (v1.length < len) {
+      v1.push('0')
+    }
+    while (v2.length < len) {
+      v2.push('0')
+    }
+    
+    for (let i = 0; i < len; i++) {
+      const num1 = parseInt(v1[i])
+      const num2 = parseInt(v2[i])
+
+      if (num1 > num2) {
+        return 1
+      } else if (num1 < num2) {
+        return -1
+      }
+    }
+
+    return 0
   }
 }
-
-
-
