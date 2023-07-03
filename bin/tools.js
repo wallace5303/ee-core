5ccdaad87ae9481e4550c3ac221b53032b21a413
gsx <530353222@qq.com>
2023-07-03 21:06:08 +0800

    del iconGen

--- bin/tools.js
+++ bin/tools.js
@@ -2,7 +2,7 @@
 
 const replaceDist = require('../tools/replaceDist');
 const encrypt = require('../tools/encrypt');
-const iconGen = require('../tools/iconGen');
+//const iconGen = require('../tools/iconGen');
 
 // argv
 const args = process.argv;
@@ -22,6 +22,6 @@ if (cmd == 'clean') {
   encrypt.clean();
 }
 
-if (cmd == 'icon') {
-  iconGen.run();
-}
+// if (cmd == 'icon') {
+//   iconGen.run();
+// }
