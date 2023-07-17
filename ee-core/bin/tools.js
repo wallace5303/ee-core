576e97d872f9d701238ac7c9e783f7f92c356e02
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-07-17 18:19:44 +0800

    ee-core & ee-bin

--- ee-core/bin/tools.js
+++ ee-core/bin/tools.js
@@ -3,6 +3,6 @@
 const chalk = require('chalk');
 
 let tip = chalk.bgYellow('Warning') + ' please use ' + chalk.blue('ee-bin') + ', See: ';
-tip += chalk.underline('https://www.kaka996.com/pages/85c531/');
+tip += chalk.underline('https://www.kaka996.com/pages/e1816f/');
 
 console.log(tip);
