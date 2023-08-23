fa763cdd7672a8cdbba7f58573aedf531f8c7ea6
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-08-23 14:02:36 +0800

    bin 优化

--- ee-bin/tools/serve.js
+++ ee-bin/tools/serve.js
@@ -8,6 +8,10 @@ const chalk = require('chalk');
 
 module.exports = {
 
+  frontendProcess: undefined,
+
+  electronProcess: undefined,
+
   /**
    * 启动前端、主进程服务
    */  
@@ -43,7 +47,34 @@ module.exports = {
     console.log(chalk.blue('[ee-bin] [frontendServe] ') + chalk.green('config:'), cfg);
 
     const frontendDir = path.join(process.cwd(), cfg.directory);
-    exec(cfg.cmd, { stdio: 'inherit', cwd: frontendDir});
+    this.frontendProcess = exec(
+      cfg.cmd, 
+      { stdio: 'inherit', cwd: frontendDir},
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(chalk.blue('[ee-bin] [frontendServe] ') + chalk.red('Error:') + error);
+          return;
+        }
+        console.log(stdout);
+        console.log(stderr);
+        console.log(chalk.blue('[ee-bin] [frontendServe] ') + chalk.green('Success'));
+      });
+
+    this.frontendProcess.stdout.on('data', (data) => {
+      console.log(chalk.blue('[ee-bin] [frontendServe] ') + `${data}`);
+    });
+    this.frontendProcess.stderr.on('data', (data) => {
+      console.error(chalk.blue('[ee-bin] [frontendServe] ') + `${data}`);
+    });
+    // this.frontendProcess.on('close', (code) => {
+    //   console.log(chalk.blue('[ee-bin] [frontendServe] ') + `child process close with code ${code}`);
+    // });
+    // this.frontendProcess.on('exit', (code, signal) => {
+    //   console.log(chalk.blue('[ee-bin] [frontendServe] ') + `child process exited with code ${code} signal ${signal}`);
+    //   console.log('electronProcess', this.electronProcess)
+      
+    // });
+    //this.frontendProcess.unref();
   },
 
   /**
@@ -57,15 +88,22 @@ module.exports = {
     const electronDir = path.join(process.cwd(), cfg.directory);
     const electronArgs = is.string(cfg.args) ? [cfg.args] : cfg.args;
     
-    // 疑问，为什么直接使用 electron，spawn会报错
+    // 疑问，为什么直接使用 electron，spawn会报错(或许是衍生shell问题，衍生的shell有系统环境变量)
     let electronProgram 
     if (cfg.cmd == 'electron') {
       electronProgram = Utils.getElectronProgram();
     }
 
-    spawn(electronProgram, electronArgs, {
+    this.electronProcess = spawn(electronProgram, electronArgs, {
       stdio: 'inherit', 
       cwd: electronDir,
+    });
+
+    this.electronProcess.on('exit', (code, signal) => {
+      console.log(`child process exited with code: ${code} , signal: ${signal}`);
+      setTimeout(() => {
+        process.exit();
+      }, 500)
     });
   },  
 
@@ -88,7 +126,7 @@ module.exports = {
     }, 1000)
 
     const frontendDir = path.join(process.cwd(), buildCfg.directory);
-    exec(
+    const buildProcess = exec(
       buildCfg.cmd, 
       { stdio: 'inherit', cwd: frontendDir, maxBuffer: 1024 * 1024 * 1024}, 
       (error, stdout, stderr) => {
