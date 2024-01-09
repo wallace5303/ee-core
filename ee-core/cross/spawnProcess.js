95aff4323b109524051a50be572c44a029a54a1f
gaoshuaixing <gaoshuaixing@bilibili.com>
2024-01-09 14:34:46 +0800

    cross 子进程退出

--- ee-core/cross/spawnProcess.js
+++ ee-core/cross/spawnProcess.js
@@ -1,13 +1,12 @@
 const EventEmitter = require('events');
 const path = require('path');
 const crossSpawn = require('cross-spawn');
-const serialize = require('serialize-javascript');
-const { app: electronApp } = require('electron');
 const Log = require('../log');
 const Ps = require('../ps');
 const Channel = require('../const/channel');
 const Helper = require('../utils/helper');
 const UtilsPargv = require('../utils/pargv');
+const EE = require('../ee');
 
 class SpawnProcess {
   constructor(host, opt = {}) {
@@ -47,28 +46,6 @@ class SpawnProcess {
     });
     this.child = coreProcess;
     this.pid = coreProcess.pid;
-    coreProcess.on('close', (code, signal) => {
-      Log.coreLogger.info(`[ee-core] [cross/process] received a close from child-process, code:${code}, signal:${signal}, pid:${this.pid}`);
-
-      // electron quit
-      if (this.config.appExit) {
-        setTimeout(() => {
-          // 进程退出前的一些清理工作
-          electronApp.quit();
-        }, 1000)
-      }
-    });
-
-    coreProcess.on('message', (m) => {
-      Log.coreLogger.info(`[ee-core] [corss/process] received a message from child-process, message: ${serialize(m)}`);
-      
-      if (!m || !m.id) {
-        return;
-      }
-
-      // 收到子进程消息，转发到 event 
-      //this.emitter.emit(m.event, m.data);
-    });
 
     coreProcess.on('exit', (code, signal) => {
       let data = {
@@ -76,6 +53,7 @@ class SpawnProcess {
       }
       this.host.emitter.emit(Channel.events.childProcessExit, data);
       Log.coreLogger.info(`[ee-core] [corss/process] received a exit from child-process, code:${code}, signal:${signal}, pid:${this.pid}`);
+      this._exitElectron();
     });
 
     coreProcess.on('error', (err) => {
@@ -84,6 +62,7 @@ class SpawnProcess {
       }
       this.host.emitter.emit(Channel.events.childProcessError, data);
       Log.coreLogger.error(`[ee-core] [corss/process] received a error from child-process, error: ${err}, pid:${this.pid}`);
+      this._exitElectron();
     });
   }
 
@@ -127,6 +106,19 @@ class SpawnProcess {
   getArgsObj() {
     const obj = UtilsPargv(this.config.args);
     return obj;
+  }
+
+  /**
+   * exit electron
+   */
+  _exitElectron(timeout = 1000) {
+    const { CoreApp } = EE;
+    if (this.config.appExit) {
+      setTimeout(() => {
+        // 进程退出前的一些清理工作
+        CoreApp.appQuit();
+      }, timeout)
+    }
   }
 }
 
