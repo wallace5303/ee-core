7b5d1637aaa7fd4c2a49296529af01ac133d2a4a
gaoshuaixing <gaoshuaixing@bilibili.com>
2024-10-28 19:10:11 +0800

    ee-bin v2

---
+++ ee-bin/vite.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig} from 'vite'
+import {resolve} from 'path';
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  define: {'process.env': {}},
+  build: {
+      minify: "esbuild",
+      lib: {
+          entry: resolve(__dirname, './src/index.ts'),
+          name: 'eebin',
+          // fileName: (format) => `index.${format}.js`,
+          fileName: `index`,
+          formats: ['es', 'cjs']
+      },
+  },
+  plugins: [dts({rollupTypes: true}),
+      // legacy({
+      //     targets: ['defaults', 'not IE 11','chrome 52'],
+      // }),
+  ],
+})
