dc080467e4d0dc6ddf11715c937ebfd16eec5c72
gaoshuaixing <gaoshuaixing@bilibili.com>
2024-12-11 10:27:46 +0800

    feat: delete invalid code

--- ee-core/jobs/index.js
+++ ee-core/jobs/index.js
@@ -1,9 +1,7 @@
 const ChildJob = require('./child');
-const RendererJob = require('./renderer');
 const ChildPoolJob = require('./child-pool');
 
 module.exports = {
   ChildJob,
-  RendererJob,
   ChildPoolJob
 };
